adc0dae9d3fd23db822954c119fb34bc09151d1f
Praveen R <praveen.ramasamy@ibm.com>
2026-05-17 22:07:23 +0530

    Update NetworkManager.kt

--- src/main/kotlin/dbus/NetworkManager.kt
+++ src/main/kotlin/dbus/NetworkManager.kt
@@ -1,8 +1,11 @@
 package org.example.dbus
 
+import org.freedesktop.dbus.DBusPath
 import org.freedesktop.dbus.ObjectPath
+import org.freedesktop.dbus.annotations.DBusInterfaceName
 import org.freedesktop.dbus.interfaces.DBusInterface
 
+@DBusInterfaceName("org.freedesktop.NetworkManager")
 interface NetworkManager: DBusInterface{
-    fun GetAllDevices(): List<ObjectPath>
+    fun GetAllDevices(): List<DBusPath>
 }
